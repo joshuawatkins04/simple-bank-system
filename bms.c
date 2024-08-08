f0f06b24f80934c31ee30d7948cb05874256aa71
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-08-08 12:17:17 +1000

    Worked primarily on the 'create_account' function. This commit has code for taking in user input to collect the email, validate email string through the 'validate_email()' function to return a value that until is the correct value will print 'successfully created'. There is still many improvements to be made to the 'validate_email()' function as described in the comments in the 'create_account()' function

--- bms.c
+++ bms.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Account {
     char name[40];
@@ -7,6 +8,21 @@ struct Account {
     int balance;
     int deposit_amount;
 };
+
+void premenu();
+void menu();
+void login();
+void create_account();
+int validate_email();
+
+int main() {
+    premenu();
+    
+    struct Account a;
+    menu();
+
+    return 0;
+}
 
 void premenu() {
     int choice;
@@ -20,31 +36,60 @@ void premenu() {
         case 2:
             create_account();
             break;
+        default:
+            printf("################ INVALID INPUT ################");
+            premenu();
     }
 };
 
 void login() {
     int account_number;
     char password[20];
+
     printf("\nEnter details below:\nAccount number: ");
     scanf("%d", &account_number);
-    printf("\nPassword: ");
+    printf("Password: ");
     scanf("%s", &password);
 };
 
 void create_account() {
+    #define MAX_LENGTH 256
+    char* email = malloc(MAX_LENGTH);
+    char user[10];
+    char domain[4];
+    int valid;
 
+    //FILE *fptr;
+    
+    /* 
+        - Need to fix email sscanf(). Function validate_email(email) is running as expected but is validating
+        slightly wrong emails such as 'jasdf@asdf' but will correctly find emails like 'sdf'
+        - Need to implement other account creation detail features such as collecting first/last name
+        password and generate 8 digit long account number
+        - Then need to store this information into Account-Details.txt by writing it to it
+    */
+
+    // Get valid details First Name, Last Name, email, Password, Auto generate an 8 digit long account number
+    printf("\nAccount Creation:\nEnter your new email: ");
+    scanf("%s", email);
+    valid = validate_email(email);
+    while (valid != 2) {\
+        printf("\n################ INVALID INPUT ################\nEnter different email: ");
+        scanf("%s", email);
+        valid = validate_email(email);
+    }
+
+    //fptr = fopen("Account-Details.txt", "w"); 
+    
+    printf("Account successfully created");
+};
+
+int validate_email(char* email) {
+    char user[10];
+    char domain[4];
+    return sscanf(email, "%[_a-zA-Z0-9.]@%[_a-zA-Z0-9.]", user, domain);
 };
 
 void menu() {
-    printf("Successfully logged in");
+    printf("\nSuccessfully logged in");
 };
-
-int main() {
-    premenu();
-    
-    struct Account a;
-    menu();
-
-    return 0;
-}
