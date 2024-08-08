5108ad8839c526b4b5caa1381a4decb44bed6a2a
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-08-08 14:08:15 +1000

    Tidied up create_account() function and stored variables in a structure called 'Account'. Added user inputs for First/Last name and Password and added function generate_account_number().

--- bms.c
+++ bms.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_LENGTH 256
 
 struct Account {
-    char name[40];
-    char password[15];
+    char first_name[15];
+    char last_name[15];
+    char password[14];
     int account_number;
-    int balance;
-    int deposit_amount;
+
+    char* email;
+    char user[10];
+    char domain[4];
+    int valid;
 };
 
 void premenu();
@@ -14,13 +19,11 @@ void menu();
 void login();
 void create_account();
 int validate_email();
+int generate_account_number();
 
 int main() {
     premenu();
-    
-    struct Account a;
     menu();
-
     return 0;
 }
 
@@ -43,52 +46,63 @@ void premenu() {
 };
 
 void login() {
-    int account_number;
-    char password[20];
+    struct Account a;
 
     printf("\nEnter details below:\nAccount number: ");
-    scanf("%d", &account_number);
+    scanf("%d", &a.account_number);
     printf("Password: ");
-    scanf("%s", &password);
+    scanf("%s", &a.password);
 };
 
+/* 
+    - Need to fix email sscanf(). Function validate_email(email) is running as expected but is validating
+    slightly wrong emails such as 'jasdf@asdf' but will correctly find emails like 'sdf'
+    - Need to fix account number generator to wider values
+    - Then need to store this information into Account-Details.txt by writing it to it
+*/
 void create_account() {
-    #define MAX_LENGTH 256
-    char* email = malloc(MAX_LENGTH);
-    char user[10];
-    char domain[4];
-    int valid;
-
-    //FILE *fptr;
+    struct Account a;
+    a.email = malloc(MAX_LENGTH);
     
-    /* 
-        - Need to fix email sscanf(). Function validate_email(email) is running as expected but is validating
-        slightly wrong emails such as 'jasdf@asdf' but will correctly find emails like 'sdf'
-        - Need to implement other account creation detail features such as collecting first/last name
-        password and generate 8 digit long account number
-        - Then need to store this information into Account-Details.txt by writing it to it
-    */
+    // Name
+    printf("\nAccount Creation:\nEnter your first and last name: ");
+    scanf("%s %s", &a.first_name, &a.last_name);
 
-    // Get valid details First Name, Last Name, email, Password, Auto generate an 8 digit long account number
-    printf("\nAccount Creation:\nEnter your new email: ");
-    scanf("%s", email);
-    valid = validate_email(email);
-    while (valid != 2) {\
+    // Email
+    printf("\nEnter your new email: ");
+    scanf("%s", a.email);
+    a.valid = validate_email(a.email);
+    while (a.valid != 2) {\
         printf("\n################ INVALID INPUT ################\nEnter different email: ");
-        scanf("%s", email);
-        valid = validate_email(email);
+        scanf("%s", a.email);
+        a.valid = validate_email(a.email);
     }
-
-    //fptr = fopen("Account-Details.txt", "w"); 
     
+    // Password
+    printf("\nEnter a new password (14 Characters Max): ");
+    scanf("%14s", &a.password);
+
+    // Randomly generated account number
+    printf("\n################ GENERATING ACC. NO. ################\n");
+    a.account_number = generate_account_number();
+    printf("Your account number is %d\n", a.account_number);
+
     printf("Account successfully created");
 };
 
 int validate_email(char* email) {
-    char user[10];
-    char domain[4];
-    return sscanf(email, "%[_a-zA-Z0-9.]@%[_a-zA-Z0-9.]", user, domain);
+    struct Account a;
+    return sscanf(email, "%[_a-zA-Z0-9.]@%[_a-zA-Z0-9.]", a.user, a.domain);
 };
+
+// In the future need to add a check to see if account number already exists
+int generate_account_number() {
+    int lower, upper, number;
+    lower = 10000000;
+    upper = 100000000;
+    number = (rand() % (upper - lower + 1)) + lower;
+    return number;
+}
 
 void menu() {
     printf("\nSuccessfully logged in");
